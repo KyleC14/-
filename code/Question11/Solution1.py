a02adeea9097550c9b9591fa707194079a46b4a6
KyleC14 <kylechenszu@foxmail.com>
2020-09-28 21:50:57 +0800

    question11 update

--- code/Question11/Solution1.py
+++ code/Question11/Solution1.py
@@ -4,6 +4,9 @@
 
 class Solution:
     def minArray(self, numbers: List[int]) -> int:
+    	#如果数组没有旋转 直接返回第一个
+        if numbers[0]<numbers[-1]:
+            return numbers[0]
         if numbers:
             for i in range(len(numbers)-1,-1,-1):
                 if numbers[i]<numbers[i-1]:
