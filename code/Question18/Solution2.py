16c36d74965c99da6a4c6b3b684d442db16be295
KyleC14 <kylechenszu@foxmail.com>
2020-09-30 00:46:31 +0800

    question18 update

--- code/Question18/Solution2.py
+++ code/Question18/Solution2.py
@@ -2,6 +2,7 @@
 原版书上应该是这样的
 删除前n-1个节点时间复杂度为O(1),删除最后一个节点时间复杂度为O(n)
 时间复杂度 (O(1)*n-1+O(n))/n = O(1)
+思路 除了最后一个节点以外 删除当前节点 要删除的节点的下一个节点原封不动复制到当前节点即可
 '''
 class Solution:
     def deleteNode(self, head: ListNode, val: ListNode) -> ListNode:
