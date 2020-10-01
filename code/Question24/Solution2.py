278e1d2e71c227ebef4c0e5462e6cd089e1287e7
KyleC14 <kylechenszu@foxmail.com>
2020-10-01 23:36:22 +0800

    question24 update

--- code/Question24/Solution2.py
+++ code/Question24/Solution2.py
@@ -1,5 +1,7 @@
 '''
 利用递归的思想，先递归到尽头获取头节点，然后返回的过程逐步重新构建链表
+注意！是递归到尾节点进行操作 想明白这一点对于理解 head.next = none 非常重要
+https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof/solution/fan-zhuan-lian-biao-yi-dong-de-shuang-zhi-zhen-jia/
 '''
 
 class Solution:
