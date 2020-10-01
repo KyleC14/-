64d93523c996f070b6126742d929041736c0c70f
KyleC14 <kylechenszu@foxmail.com>
2020-10-02 01:13:54 +0800

    question25 update

--- code/Question25/Solution1.py
+++ code/Question25/Solution1.py
@@ -4,15 +4,18 @@
 '''
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        #如果两个链表有一为空 返回剩下的那个
         if l1 == None:
             return l2
         elif l2 == None:
             return l1
+        #选出当前的头结点
         if l1.val<=l2.val:
             head = l1
             l1 = l1.next
         else:
             head = l2
             l2 = l2.next
+        #当前头结点的下一个节点由下一次递归决定
         head.next = self.mergeTwoLists(l1,l2)
         return head
