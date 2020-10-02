d99cefa54a675c903ddb5030fba27009420e5938
KyleC14 <kylechenszu@foxmail.com>
2020-10-03 01:20:49 +0800

    question28 update

--- code/Question28/Solution3.py
+++ code/Question28/Solution3.py
@@ -14,3 +14,19 @@ class Solution:
         if not root:
             return True
         return rec(root.left,root.right)
+
+'''
+Solution1 dfs的拓展
+这版代码易于理解
+'''
+class Solution2:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def dfs(A,B):
+            if not A and not B:
+                return True
+            elif not A or not B:
+                return False
+            return A.val == B.val and dfs(A.left,B.right) and dfs(A.right,B.left)
+        if not root:
+            return True
+        return dfs(root.left,root.right)
